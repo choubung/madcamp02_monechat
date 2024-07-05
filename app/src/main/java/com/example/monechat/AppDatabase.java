eed23d8498de59301fb87b3df27c81a985bb2ebc
choubung <choubung@sookmyung.ac.kr>
2024-07-05 15:12:54 +0900

    240705 소비/수입 로컬DB (1)

--- app/src/main/java/com/example/monechat/AppDatabase.java
+++ app/src/main/java/com/example/monechat/AppDatabase.java
@@ -1,7 +1,8 @@
 package com.example.monechat;
 
-
+import android.content.Context;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
@@ -10,4 +11,19 @@ import androidx.room.TypeConverters;
 public abstract class AppDatabase extends RoomDatabase {
     public abstract ExpenseDao expenseDao();
     public abstract IncomeDao incomeDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "app_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
