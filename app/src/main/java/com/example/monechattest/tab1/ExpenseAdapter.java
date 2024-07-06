cf5861d350c53d9440f3e7a33c70dea2e977bf1d
choubung <choubung@sookmyung.ac.kr>
2024-07-06 18:03:19 +0900

    240706 ExpenseAdapter 붙임

---
+++ app/src/main/java/com/example/monechattest/tab1/ExpenseAdapter.java
@@ -0,0 +1,124 @@
+package com.example.monechattest.tab1;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.monechattest.R;
+
+import java.util.ArrayList;
+
+public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHolder> {
+    Context context;
+    ArrayList<ExpenseItem> expenseItems;
+    OnItemClickListener clickListener;
+    OnItemLongClickListener longClickListener;  // 길게 누르기 리스너 추가
+
+    public ExpenseItem getItem(int position) {
+        return expenseItems.get(position);
+    }
+
+    //아이템 추가와 삭제를 어댑터에서 직접 관리
+    public void addItem(ExpenseItem item) {
+        expenseItems.add(item);
+        notifyItemInserted(expenseItems.size() - 1);
+    }
+
+    public void removeItem(int position) {
+        expenseItems.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(ViewHolder holder, View view, int position);
+    }
+
+    public interface OnItemLongClickListener {  // 길게 누르기 리스너 인터페이스 추가
+        void onItemLongClick(ViewHolder holder, View view, int position);
+    }
+
+    public ExpenseAdapter(Context context, ArrayList<ExpenseItem> expenseItems) {
+        this.context = context;
+        this.expenseItems = expenseItems;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.expense_item, parent, false);
+        return new ViewHolder(itemView, clickListener, longClickListener);  // 생성자에 리스너 전달
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ExpenseItem item = expenseItems.get(position);
+
+        holder.description.setText(item.getDescription().toString());
+        holder.date.setText(item.getDate().toString()); // TODO:date를 텍스트로 어떻게 가져오는 지 알아볼 필요 있음
+        holder.category.setText(item.getCategory().toString());
+
+        holder.amount.setText(item.getAmount().toString());
+        // TODO: 현명소비 버튼 어떻게 연결할건지 좀 더 생각해야됨
+    }
+
+    @Override
+    public int getItemCount() {
+        return expenseItems.size();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.clickListener = listener;
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener listener) {  // 길게 누르기 리스너 설정 메서드
+        this.longClickListener = listener;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView categoryImage;
+        TextView date, category, description, amount;
+        Button isSmartExpense; // TODO: 현명 소비 화면에 어떤 방식으로 띄울 건지 구체적으로 정해야함 (drawable)
+
+        public ViewHolder(@NonNull View itemView, final OnItemClickListener clickListener, final OnItemLongClickListener longClickListener) {  // 생성자에서 리스너 전달받음
+            super(itemView);
+
+            categoryImage = itemView.findViewById(R.id.categoryImage);
+
+            description = itemView.findViewById(R.id.description);
+            date = itemView.findViewById(R.id.date);
+            category = itemView.findViewById(R.id.category);
+
+            amount = itemView.findViewById(R.id.amount);
+            isSmartExpense = itemView.findViewById(R.id.isSmartExpense);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if (clickListener != null && position != RecyclerView.NO_POSITION) {
+                        clickListener.onItemClick(ViewHolder.this, view, position);
+                    }
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {  // 길게 누르기 리스너 설정
+                @Override
+                public boolean onLongClick(View view) {
+                    int position = getAdapterPosition();
+                    if (longClickListener != null && position != RecyclerView.NO_POSITION) {
+                        longClickListener.onItemLongClick(ViewHolder.this, view, position);
+                        return true;  // 이벤트 소비
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/monechattest/tab1/IncomeFragment.java
+++ app/src/main/java/com/example/monechattest/tab1/IncomeFragment.java
@@ -3,14 +3,25 @@ package com.example.monechattest.tab1;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.monechattest.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class IncomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    ExpenseAdapter adapter;
+    ArrayList<ExpenseItem> expenseItems = new ArrayList<>();
+    FloatingActionButton fab;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_2, container, false);
