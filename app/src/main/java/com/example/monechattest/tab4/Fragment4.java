88e89cfcbdb5fc7ebb5ac9bb7889090960867f51
choubung <choubung@sookmyung.ac.kr>
2024-07-06 17:41:33 +0900

    240706 소비 리스트 틀 추가 및 프4 만듦

---
+++ app/src/main/java/com/example/monechattest/tab4/Fragment4.java
@@ -0,0 +1,20 @@
+package com.example.monechattest.tab4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.monechattest.R;
+
+public class Fragment4 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_4, container, false);
+
+        return rootView;
+    }
+}
