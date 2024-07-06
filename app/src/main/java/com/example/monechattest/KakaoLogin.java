3d6b621068b0ca6ebd5c76648d5de0efba7bc41a
choubung <choubung@sookmyung.ac.kr>
2024-07-06 21:18:00 +0900

    240706 카카오 로그인 기반 마련 코드 내부 앱키 처리 + 플랫폼에서 개발자들 해시등록만 하면 됨 해시는 카카오톡으로 보내놓음

--- app/src/main/java/com/example/monechattest/KakaoLogin.java
+++ app/src/main/java/com/example/monechattest/KakaoLogin.java
@@ -1,19 +1,49 @@
 package com.example.monechattest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.kakao.sdk.auth.AuthApiClient;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
 
 public class KakaoLogin extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kakao_login);
 
+        // 카카오 SDK 초기화
+        KakaoSdk.init(this, "카카오 앱키");
+
+        Button kakaoLoginButton = findViewById(R.id.kakao_login_button);
+        kakaoLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserApiClient.getInstance().loginWithKakaoTalk(KakaoLogin.this, (oAuthToken, throwable) -> {
+                    if (throwable != null) {
+                        Log.e("KakaoLogin", "로그인 실패", throwable);
+                    } else if (oAuthToken != null) {
+                        Log.i("KakaoLogin", "로그인 성공");
+                        moveToMainActivity();
+                    }
+                    return null;
+                });
+            }
+        });
+    }
+
+    private void moveToMainActivity() {
+        Intent intent = new Intent(KakaoLogin.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // LoginActivity 종료
     }
 }
