d99905c668bf0aa1e4bac882805fbf53e7cd7c80
choubung <choubung@sookmyung.ac.kr>
2024-07-06 18:07:18 +0900

    240706 모든 클래스 껍데기 다 만듦

---
+++ app/src/main/java/com/example/monechattest/tab1/AddExpenseDetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.monechattest.tab1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.monechattest.R;
+
+public class AddExpenseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense_detail);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/monechattest/tab1/AddIncomeDetailActivity.java
@@ -0,0 +1,21 @@
+package com.example.monechattest.tab1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.monechattest.R;
+
+public class AddIncomeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_income_detail);
+
+    }
+}
