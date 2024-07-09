c4e215d2bc997c2e3eed1873883f6188b5f88687
choubung <choubung@sookmyung.ac.kr>
2024-07-09 20:25:29 +0900

    240709 색 바꿈

--- app/src/main/java/com/example/monechattest/database/ExpenseDao.java
+++ app/src/main/java/com/example/monechattest/database/ExpenseDao.java
@@ -36,6 +36,8 @@ public interface ExpenseDao {
     @Query("SELECT strftime('%Y-%m', date) AS month, SUM(CAST(amount AS REAL)) AS total FROM expense GROUP BY month ORDER BY month")
     List<MonthlyExpense> getMonthlyExpenses(); // gpt코드 (240707)
 
+
+
     @Query("SELECT SUM(CAST(amount AS REAL)) AS total FROM expense WHERE strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth")
     double getTotalForMonth(String yearMonth); // gpt코드 (240707)
 
